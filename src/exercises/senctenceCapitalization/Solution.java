29f5d713b6f3bfcdacfd7512ca12a546226ad732
Aleksei Lazunin <gegcuk@gmail.com>
2024-07-05 00:23:33 +0100

    Chapter 10. Capitalize sentence. Need to fix bug.

---
+++ src/exercises/senctenceCapitalization/Solution.java
@@ -0,0 +1,50 @@
+package exercises.senctenceCapitalization;
+
+public class Solution {
+    public String capitalize(String s){
+        StringBuilder resultString = new StringBuilder();
+        boolean spaceFlag = false;
+        s = s.trim();
+        if(Character.isLowerCase(s.charAt(0))) s = s.replace(s.charAt(0), Character.toUpperCase(s.charAt(0)));
+        for(char ch : s.toCharArray()){
+            if(spaceFlag && Character.isLowerCase(ch)) ch = Character.toUpperCase(ch);
+            if(ch == ' ') {
+                spaceFlag = true;
+                resultString.append(ch);
+            }
+            else {
+                resultString.append(ch);
+                spaceFlag = false;
+            }
+        }
+        return resultString.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test cases with expected results
+        Object[][] testCases = {
+                {"hello world", "Hello World"},
+                {"java is fun", "Java Is Fun"},
+                {"capitalize each word", "Capitalize Each Word"},
+                {"this is a test", "This Is A Test"},
+                {"open ai", "Open Ai"},
+                {"a b c", "A B C"},
+                {"singleword", "Singleword"},
+                {"  leading spaces", "Leading Spaces"},
+                {"trailing spaces  ", "Trailing Spaces"}
+        };
+
+        // Testing the capitalize function
+        for (Object[] testCase : testCases) {
+            String testString = (String) testCase[0];
+            String expectedResult = (String) testCase[1];
+            String result = solution.capitalize(testString);
+            System.out.println("Original: \"" + testString + "\"");
+            System.out.println("Capitalized: \"" + result + "\"");
+            System.out.println("Expected: \"" + expectedResult + "\"");
+            System.out.println();
+        }
+    }
+}
