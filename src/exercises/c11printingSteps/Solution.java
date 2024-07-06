2d2f6b3e7de448e77e8bde402c51ef3f319dc2da
Aleksei Lazunin <gegcuk@gmail.com>
2024-07-06 23:14:48 +0100

    Chapter 11. Printing steps corrected

--- src/exercises/c11printingSteps/Solution.java
+++ src/exercises/c11printingSteps/Solution.java
@@ -3,15 +3,24 @@ package exercises.c11printingSteps;
 public class Solution {
 
     public static void printSteps(int n){
-        for(int i = 0; i < n; i++){
-            for(int x = 0; x < i; x++){
-                System.out.println("#".repeat(x));
+        for(int i = 1; i <= n; i++){
+            for(int x = 1; x <= i; x++){
+                int stringLength = i;
+                StringBuilder s = new StringBuilder();
+                for(int j = 0; j < x; j++){
+                    s.append("#");
+                    stringLength--;
+                }
+                for(int j = 0; j < stringLength; j++){
+                    s.append(" ");
+                }
+                System.out.println(s);
             }
         }
     }
 
     public static void main(String[] args) {
-        printSteps(10);
+        printSteps(3);
     }
 
 }
